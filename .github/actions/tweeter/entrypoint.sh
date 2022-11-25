2dd883549e2405b88d59aae7f18b97fb7ead984f
Richard Anthony <public.rant@pm.me>
2022-11-26 09:15:30 +1100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! wip: testtest

--- .github/actions/tweeter/entrypoint.sh
+++ .github/actions/tweeter/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+calcurse -D . -Q #--days $days --format-recur-event '  %n\n' --format-apt '  %n' | yq [.[]] | jq '.[] | split(" ")' | jq -s flatten
+
 days=1 # $GITHUB_INPUT TODO
 echo "schedule=$(calcurse -D . -Q --days $days --format-recur-event '  %n\n' --format-apt '  %n' | yq [.[]] | jq '.[] | split(" ")' | jq -s flatten)" >> $GITHUB_OUTPUT
