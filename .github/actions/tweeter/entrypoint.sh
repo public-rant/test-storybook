f6be10fafbf80cb46322d11184d1fc9b07e5136f
Richard Anthony <public.rant@pm.me>
2022-11-25 17:47:50 +1100

    wip: tweeter

---
+++ .github/actions/tweeter/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+days=1 # $GITHUB_INPUT TODO
+echo "schedule="$(calcurse -D . --format-todo '  %n\n' --days $days -Q --format-recur-apt '  %n\n' --format-event '  %n\n' | yq) >> $GITHUB_OUTPUT
